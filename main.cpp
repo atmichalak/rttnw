ef4a44c948621d2db95c2ebe07a1ca8fe82cc622
Alex Michalak <atmichalak@gmail.com>
2021-05-12 14:46:24 -0400

    Complete 2.5 - Add image1.ppm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "color.hpp"
 #include "hittable_list.hpp"
 #include "material.hpp"
+#include "moving_sphere.hpp"
 #include "rtweekend.hpp"
 #include "sphere.hpp"
 
@@ -59,7 +60,8 @@ hittable_list random_scene() {
                     // Diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0, 0.5), 0);
+                    world.add(make_shared<moving_sphere>(center, center2, 0.0, 1.0, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // Metal
                     auto albedo = color::random(0.5, 1);
@@ -89,10 +91,9 @@ hittable_list random_scene() {
 
 int main() {
     // Image
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 1200;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
@@ -104,8 +105,9 @@ int main() {
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
     auto aperture = 0.1;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
