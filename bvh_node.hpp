fe58459395889c264d1dd5b7374d61e62c19b303
Alex Michalak <atmichalak@gmail.com>
2021-05-16 21:31:18 -0400

    Complete RTTNW -- Missing last image

--- bvh_node.hpp
+++ bvh_node.hpp
@@ -31,7 +31,30 @@ public:
     aabb box;
 };
 
-bvh_node::bvh_node(std::vector<shared_ptr<hittable>>& src_objects,
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b)) {
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+    }
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 2);
+}
+
+bvh_node::bvh_node(const std::vector<shared_ptr<hittable>>& src_objects,
                    size_t start, size_t end, double time0, double time1) {
     auto objects = src_objects;     // Create a modified array of the source scene objects.
 
@@ -47,7 +70,7 @@ bvh_node::bvh_node(std::vector<shared_ptr<hittable>>& src_objects,
     } else if (object_span == 2) {
         if (comparator(objects[start], objects[start+1])) {
             left = objects[start];
-            right = objects[start+1]
+            right = objects[start+1];
         } else {
             left = objects[start+1];
             right = objects[start];
@@ -81,29 +104,6 @@ bool bvh_node::hit(const ray &r, double t_min, double t_max, hit_record& rec) co
 bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const {
     output_box = box;
     return true;
-}
-
-inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
-    aabb box_a;
-    aabb box_b;
-
-    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b)) {
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-    }
-
-    return box_a.min().e[axis] < box_b.min().e[axis];
-}
-
-bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 0);
-}
-
-bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable b) {
-    return box_compare(a, b, 1);
-}
-
-bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_z_compare(a, b, 2);
 }
 
 #endif //RTTNW_BVH_NODE_HPP
